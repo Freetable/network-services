3273166a71fd1e4dd4b8362e2ab233f7d2893100
Fuzzy <fuzzy@sinatra.freetable.info>
2013-12-13 00:24:53 -0800

    nginx incomming

--- server/main.rb
+++ server/main.rb
@@ -1,6 +1,17 @@
+###############################################################################
+#
+# Freetable Network Services Application Layer Server
+# Created By: Stephanie Sunshine
+# (C) The Freetable Project
+#
+###############################################################################
+
 require 'sinatra'
 require 'sinatra/reloader' if development?
+# All of our POST's
 
+
+# All of our GET's
 get '/' do
   '/'
 end
