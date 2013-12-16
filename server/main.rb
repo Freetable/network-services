fc314343a70009150cc6cf51a8e1931eb3ce9495
thegleek <thegleek@thegleek.com>
2013-12-16 09:41:18 -0600

    lol spacing/tabbing was way off, fixed

--- server/main.rb
+++ server/main.rb
@@ -22,12 +22,12 @@ require_relative('conf')
 
 # Setup a database connection pool
 @@dbh_pool = ConnectionPool.new( :size => 4 ) { Mysql2::Client.new(	:host      => HOST_POOL, 
-																	:database  => DB_NAME, 
-																	:username  => DB_USER, 
-																	:password  => DB_PASS, 
-																	:flags     => Mysql2::Client::MULTI_STATEMENTS, 
-																	:reconnect => true, 
-																	:encoding  => 'utf8' ) }
+									:database  => DB_NAME, 
+									:username  => DB_USER, 
+									:password  => DB_PASS, 
+									:flags     => Mysql2::Client::MULTI_STATEMENTS, 
+									:reconnect => true, 
+									:encoding  => 'utf8' ) }
 
 # Dynamic loader, anything in api-functions will be dynamically loaded inline at runtime 
 Dir.foreach("api-functions") {|s| require_relative("api-functions/"+s) if s !~ /^\./ }
