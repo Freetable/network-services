33dc0bf2316b4bf56a95c8e15d5aa76564423ae3
thegleek <thegleek@thegleek.com>
2013-12-16 10:39:14 -0500

    pretty printed dbh_pool cuz imma anal

--- server/main.rb
+++ server/main.rb
@@ -7,7 +7,6 @@
 ###############################################################################
 
 # Required modules
-
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'sinatra/respond_to'
@@ -19,19 +18,21 @@ require 'connection_pool'
 require 'sequel'
 
 # Conf import
-
 require_relative('conf')
 
 # Setup a database connection pool
-
-@@dbh_pool = ConnectionPool.new( :size => 4 ) { Mysql2::Client.new(:host => HOST_POOL, :database => DB_NAME, :username => DB_USER, :password => DB_PASS, :flags => Mysql2::Client::MULTI_STATEMENTS, :reconnect => true, :encoding => 'utf8' ) }
+@@dbh_pool = ConnectionPool.new( :size => 4 ) { Mysql2::Client.new(	:host      => HOST_POOL, 
+																	:database  => DB_NAME, 
+																	:username  => DB_USER, 
+																	:password  => DB_PASS, 
+																	:flags     => Mysql2::Client::MULTI_STATEMENTS, 
+																	:reconnect => true, 
+																	:encoding  => 'utf8' ) }
 
 # Dynamic loader, anything in api-functions will be dynamically loaded inline at runtime 
-
 Dir.foreach("api-functions") {|s| require_relative("api-functions/"+s) if s !~ /^\./ }
 
 # Catch all for what doesn't get caught first
-
 get '*' do
 	redirect to('/')
 end
