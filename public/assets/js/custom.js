cb2b26003889621acab113a2ee0895bf45362b82
Someone@Sinatra <someone@sinatra.freetable.info>
2013-12-17 10:19:52 -0800

    css editions, javascript cleanup for public

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -20,3 +20,28 @@ function login ( nickname, password, callback ){
 			success: callback
 		});
 }
+
+// Startup stuff for various pages
+
+$(function() {  
+// Main signin button
+	$("#signin").click(function() {  
+    login( $('#nickname').val(), CryptoJS.SHA512($('#password').val()), function( data ) {  
+    data = data.shift();
+    if( typeof data['WWUSERID'] != 'undefined') {
+      $.cookie('WWUSERID', data['WWUSERID'], { expires: 30 });
+      $.cookie('sessionid', data['sessionid'], { expires: 30 });
+      window.location.("main");
+    }else{
+    $('<div class="alert alert-danger">Invalid Login <button class="close" data-dismiss="alert" aria-hidden="true" type="button">&times;</a></div>').prependTo("#body");
+    }
+        
+    });  
+    return false;
+	});  
+
+
+
+
+});  
+
