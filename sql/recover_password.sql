01d3efb756050f39c2f9c679d5fbd1bd64844c6b
Someone@Sinatra <someone@sinatra.freetable.info>
2013-12-18 03:04:47 -0800

    Stored Procedure updates for forgot password.  Removing get_email_from_nick, get_nick_from_email.  Created forgot_password.sql

---
+++ sql/recover_password.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS recover_user$$
+CREATE PROCEDURE recover_user (search VARCHAR(96))
+  BEGIN
+		IF ((SELECT COUNT (*) FROM users WHERE email LIKE search OR nickname LIKE search) > 0) THEN
+    UPDATE users SET sessionid = create_random_hash(128) WHERE email LIKE search OR nickname LIKE search;
+    SELECT WWUSERID, sessionid, first_name, last_name, nickname, email FROM users WHERE email LIKE search OR nickname LIKE search;
+		END IF;
+  END$$
+  
+DELIMITER ;
