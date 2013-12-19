8af38aa5c3dce556d0b373f5aa594e50dc0064b3
Someone@Sinatra <someone@sinatra.freetable.info>
2013-12-19 09:04:34 -0800

    updating set_user_email.sql to include sessionid in calling function

--- sql/set_user_email.sql
+++ sql/set_user_email.sql
@@ -1,9 +1,9 @@
 DELIMITER $$
 
 DROP PROCEDURE IF EXISTS set_user_email$$
-CREATE PROCEDURE set_user_email (uid CHAR(128), eemail VARCHAR(96))
+CREATE PROCEDURE set_user_email (uid CHAR(128), eemail VARCHAR(96), sid CHAR(128))
   BEGIN
-    UPDATE users SET email = eemail WHERE WWUSERID = uid; 
+    UPDATE users SET email = eemail WHERE WWUSERID = uid AND sessionid = sid; 
   END$$
   
 DELIMITER ;
