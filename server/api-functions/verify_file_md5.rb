898795d87ca7f73b7e630d98bb41d68fb601316c
Someone@Sinatra <someone@sinatra.freetable.info>
2013-12-24 19:44:34 -0800

    more fixes

--- server/api-functions/verify_file_md5.rb
+++ server/api-functions/verify_file_md5.rb
@@ -1,5 +1,5 @@
 post '/api/verify_file_md5' do
-  my_fields = [ 'wwfileid', 'hash' ]
+  my_fields = [ 'hash' ]
   values = []
   my_fields.each { |field| return '-0' if(params[field].nil?) ; values.push(params[field]) }
   query_db('verify_file_md5', values).to_json
--- server/api-functions/verify_file_sha1.rb
+++ server/api-functions/verify_file_sha1.rb
@@ -1,5 +1,5 @@
 post '/api/verify_file_sha1' do
-  my_fields = [ 'wwfileid', 'hash' ]
+  my_fields = [ 'hash' ]
   values = []
   my_fields.each { |field| return '-0' if(params[field].nil?) ; values.push(params[field]) }
   query_db('verify_file_sha1', values).to_json
