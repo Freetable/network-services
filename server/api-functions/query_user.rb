bc0c67c41a5f4276c0bdff2920089214bb4e174a
Stephanie Sunshine <stephanie@nirvanadesktop.com>
2013-12-27 21:18:14 -0800

    cleaning up things

--- server/api-functions/query_user.rb
+++ server/api-functions/query_user.rb
@@ -1,10 +1,17 @@
 get '/api/query_user.pls' do
   response.headers["Access-Control-Allow-Origin"] = "*"
   response.headers["Access-Control-Allow-Methods"] = "GET"
-  cache_control :public, :max_age => 900
+#  cache_control :public, :max_age => 900
+
   my_fields = [ 'wwuserid' ]
   values = []
-  my_fields.each { |field| return '-0'.to_json if(params[field].nil?) ; values.push(params[field]) }
+  fail = false
+  my_fields.each { |field| if(params[field].nil?) 
+				fail = true
+				break
+				end
+			values.push(params[field]) }
+  return ['-0'=>'-0'].to_json if fail
   query_db('query_user', values).to_json
 end
 
--- server/api-functions/verify_user.rb
+++ server/api-functions/verify_user.rb
@@ -1,12 +1,18 @@
 get '/api/verify_user.pls' do
   response.headers["Access-Control-Allow-Origin"] = "*"
   response.headers["Access-Control-Allow-Methods"] = "GET"
-  cache_control :public, :max_age => 60
+#  cache_control :public, :max_age => 60
   etag ''
 
   my_fields = [ 'wwuserid', 'sessionid' ]
   values = []
-  my_fields.each { |field| return '-0' if(params[field].nil?) ; values.push(params[field]) }
+  fail = false
+  my_fields.each { |field|  if(params[field].nil?) 
+				fail = true
+				break
+				end
+			values.push(params[field]) }
+  return ['-0'=>'-0'].to_json if fail
   query_db('verify_user', values).to_json
 end
 
