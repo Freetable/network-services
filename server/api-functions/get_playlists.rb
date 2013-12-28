912a112b6669166761357a8cb6e98d40ffcdf2b9
Someone@Sinatra <someone@sinatra.freetable.info>
2013-12-28 00:16:42 -0800

    fixing does_user_exist.sql

--- server/api-functions/get_playlists.rb
+++ server/api-functions/get_playlists.rb
@@ -1,7 +1,16 @@
-post '/api/get_playlists' do
+post '/api/get_playlists.pls' do
+  response.headers["Access-Control-Allow-Origin"] = "*"
+  response.headers["Access-Control-Allow-Methods"] = "POST"
+#  cache_control :public, :max_age => 15
   my_fields = [ 'wwuserid', 'sessionid', ]
   values = []
-  my_fields.each { |field| return '-0' if(params[field].nil?) ; values.push(params[field]) }
+  fail = false
+  my_fields.each { |field| if(params[field].nil?)
+                                fail = true
+                                break
+                                end
+                        values.push(params[field]) }
+  return ['-0'=>'-0'].to_json if fail
   query_db('get_playlists', values).to_json
 end
 
--- sql/does_user_exist.sql
+++ sql/does_user_exist.sql
@@ -4,7 +4,9 @@ DROP PROCEDURE IF EXISTS does_user_exist$$
 CREATE PROCEDURE does_user_exist (nname VARCHAR(32), eemail VARCHAR(96))
   BEGIN
 	IF (SELECT COUNT(*) FROM users WHERE nickname LIKE nname OR email LIKE eemail) > 0 THEN
-	    SELECT WWUSERID, nickname, email FROM users WHERE nickname LIKE nname OR email LIKE eemail;
+	    SELECT 1;
+  ELSE
+			SELECT 0;
 	END IF;
   END$$
   
