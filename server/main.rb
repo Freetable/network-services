129dfe5ade768b252c73c723780c3ce21e02092b
Stephanie Sunshine <stephanie@nirvanadesktop.com>
2014-01-11 09:22:47 -0800

    and thus starts the quest for return standardizations to become json complaint

--- server/main.rb
+++ server/main.rb
@@ -20,6 +20,11 @@ require 'mandrill'
 # Conf import
 require_relative('conf')
 
+FUNCTIONFAIL  = '{"result": "-0"}'
+RETURNFAIL    = '{"result": "0"}'
+RETURNSUCCESS = '{"result": "1"}'
+
+
 # Setup a database connection pool
 @@dbh_pool = ConnectionPool.new( :size => 4 ) { Mysql2::Client.new(	:host      => HOST_POOL, 
 									:database  => DB_NAME, 
